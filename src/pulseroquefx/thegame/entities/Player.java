4038277a792adcd70f9272a08c4e3e22ef74165d
Qmppu842 <omppu842@gmail.com>
2018-12-01 05:52:47 +0200

    Added basic things for making player.

---
+++ src/pulseroquefx/thegame/entities/Player.java
@@ -0,0 +1,20 @@
+package pulseroquefx.thegame.entities;
+
+import java.awt.Point;
+
+/**
+ *
+ * @author Qmppu842
+ */
+public class Player {
+
+    private final char SYMBOL = '@';
+    private Point position;
+    private boolean isThisPassable;
+
+    public Player() {
+        position = new Point(1, 1);
+        isThisPassable = false;
+    }
+
+}
